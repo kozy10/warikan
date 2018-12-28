4dd8c9d069690c06f9e080fc91cd6df1d335d83a
kozy10 <kentaro1002vb@gmail.com>
2018-12-28 21:51:51 +0900

    Modify application controller

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,22 +1,6 @@
 class LinebotController < ApplicationController
-  protect_from_forgery :except => [:callback, :index]
-  require 'sinatra'   # gem 'sinatra'
-  require 'line/bot'  # gem 'line-bot-api'
-
-  def index
-  end
-
-  def client
-    @client ||= Line::Bot::Client.new do |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-      config.http_options = {
-        open_timeout: 5,
-        read_timeout: 5,
-      }
-    end
-  end
-
+  protect_from_forgery :except => [:callback]
+  
   def reply_text(event, texts)
     texts = [texts] if texts.is_a?(String)
     client.reply_message(
@@ -35,13 +19,6 @@ class LinebotController < ApplicationController
 
 
   def callback
-    body = request.body.read
-
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
-
     events = client.parse_events_from(body)
 
     events.each do |event|
