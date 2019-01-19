a9130949535690ea9ee93bce5efb979bd9b8be2b
kozy10 <kentaro1002vb@gmail.com>
2019-01-19 10:09:40 +0900

    delete flash message when join

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,8 @@ class UsersController < ApplicationController
 			room = Room.find_by(room_id: @user.room_id)
 			room.number_of_members += 1
 			room.save
-			flash[:notice] = "参加しました。"
 			redirect_to users_complete_path
 		else
-			flash[:notice] = "すでに参加しています。"
 			redirect_to users_error_path
 		end
 	end
